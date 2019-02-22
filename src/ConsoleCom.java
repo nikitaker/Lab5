44991af42e0df450998359efd843cd3d80a7b58b
nikitaker <nikitaker@mail.ru>
2019-02-22 22:52:13 +0300

    р

--- src/ConsoleCom.java
+++ src/ConsoleCom.java
@@ -10,7 +10,7 @@ public class ConsoleCom {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int i = 0;
         while(true){
-            System.out.println("Введите команду");
+            System.out.println("Введите команду (для помощи наберите help)");
 
             String line = br.readLine();
             if(Arrays.asList(line.split(" ")).contains("remove")){
@@ -33,8 +33,25 @@ public class ConsoleCom {
                 catch (NumberFormatException e){ System.out.println("В скобках должно находиться числовое значение"); }
             }
 
+            if(Arrays.asList(line.split(" ")).contains("insert")){
 
-            if (line.equals("exit")){break;}
+            }
+            if (line.equals("help")){
+                System.out.println("remove_lower {String key} :удалить из коллекции все элементы, ключ которых меньше, чем заданный");
+                System.out.println("insert {String key} {element} :добавить новый элемент с заданным ключом");
+                System.out.println("show: вывести в стандартный поток вывода все элементы коллекции в строковом представлении");
+                System.out.println("info: вывести в стандартный поток вывода информацию о коллекции (тип, дата инициализации, количество элементов и т.д.)");
+                System.out.println("remove {String key}: удалить элемент из коллекции по его ключу");
+                System.out.println("add_if_min {element}: добавить новый элемент в коллекцию, если его значение меньше, чем у наименьшего элемента этой коллекции");
+                System.out.println("add_if_max {element}: добавить новый элемент в коллекцию, если его значение превышает значение наибольшего элемента этой коллекции");
+                System.out.println("exit: выход из программы с сохранением");
+            }
+
+
+            if (line.equals("exit")){
+                FileReader.endProg(System.getenv("KARLSON"));
+                break;}
+
             if (line.equals("show")){System.out.println(Main.collection.values());}
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,21 @@ public class Main {
     public static void main(String[] args)
     {
         String variant = "10149";
-        //String path = System.getenv("KARLSON_PATH");
-        String path = "/home/hakerman/collection.json";
-        String path2 = "/home/hakerman/collection.json";
+        String path = System.getenv("KARLSON");
+        //String path = "/home/hakerman/collection.json";
+        //String path2 = "/home/hakerman/collection.json";
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.out.println();
+                FileReader.endProg(path);
+            } catch (Exception e) {
+                System.err.println("************* YARIK BOCHOK POTIK  **************");
+            }
+        }));
 
         FileReader.generate(path);
-        FileReader.endProg(path2);
+        //FileReader.endProg(path);
         System.out.println(collection.keySet());
         try {
             ConsoleCom.start();
